34f02f79cbd37db1211e1da061bc7e7c09409be0
Margaret A <margaretalexia24@gmail.com>
2020-12-05 16:37:34 -0500

    Add project files.

---
+++ intento2ado/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel;
+using intento2ado.Models;
+
+namespace intento2ado.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        MINIMARKETEntities _db;
+        public HomeController()
+        {
+            _db = new MINIMARKETEntities();
+        }
+
+        public ActionResult Index()
+        {
+            ViewData.Model = _db.prod.ToList();
+            ViewData.Model = (from m in _db.prod select m).ToList();
+            //var categs = _db.categ.ToList();
+            //categs = (from m in _db.categ select m).ToList();
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ intento2ado/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="intento2ado.MvcApplication" Language="C#" %>
